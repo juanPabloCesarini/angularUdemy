b158fead57c5e32fab54b52ef6d53a5f52b6e087
Juan Pablo Cesarini <jcesarini@matelab.com.ar>
2022-01-04 20:31:52 -0300

    property binding

--- holamundo/src/app/app.component.ts
+++ holamundo/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'holamundo';
+  nombre = "Juan";
+  textoPlaceholder = "Escribí tu nombre";
+  deshabilitado = true;
+  imgSrc = "https://cdn2.civitatis.com/argentina/mar-del-plata/galeria/big/lobos-marinos-mar-plata.jpg";
+
+  constructor() {
+    setInterval(() => this.nombre = 'Pablo', 3000);
+    setInterval(() => this.deshabilitado = false, 3000);
+  }
+
+  getSuma(num1: number, num2: number) {
+    return num1 + num2;
+  }
 }
