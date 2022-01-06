b72ecad0010c4ce7a9b95fe2559d27f55e0bf3d4
Juan Pablo Cesarini <jcesarini@matelab.com.ar>
2022-01-06 20:58:01 -0300

    agregar tareas

--- app-tareas/src/app/components/tareas/tareas.component.ts
+++ app-tareas/src/app/components/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/Tarea';
 
 @Component({
   selector: 'app-tareas',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tareas.component.css']
 })
 export class TareasComponent implements OnInit {
+  listTareas: Tarea[] = [];
+  nombreTarea: string = '';
 
   constructor() { }
 
+  agregarTarea() {
+
+    // Se crea el objeto tarea
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado : false,
+    };
+
+    // Se agrega en un array
+    this.listTareas.push(tarea);
+
+    // Se resetea el input
+    this.nombreTarea = '';
+  }
   ngOnInit(): void {
   }
 
---
+++ app-tareas/src/app/models/Tarea.ts
@@ -0,0 +1,9 @@
+export class Tarea{
+  nombre: string;
+  estado: boolean;
+
+  constructor(nombre: string, estado: boolean) {
+    this.estado = estado;
+    this.nombre = nombre;
+  }
+}
