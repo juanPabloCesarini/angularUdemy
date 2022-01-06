064f4220f0d8a9a66431a6513abf3b10359da6e2
Juan Pablo Cesarini <jcesarini@matelab.com.ar>
2022-01-06 16:45:19 -0300

    app terminada

--- app-dados/src/app/app.component.ts
+++ app-dados/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-dados';
+
+  dadoIzq = "../assets/img/dice2.png";
+  dadoDer = "../assets/img/dice4.png";
+  num1: number = 0;
+  num2: number = 1;
+
+  tirarDados(): void{
+    this.num1 = Math.round(Math.random()*5)+1;
+    this.num2 = Math.round(Math.random()*5)+1;
+    console.log(this.num1);
+    console.log(this.num2);
+    this.dadoIzq = "../assets/img/dice" + this.num1 + ".png";
+    this.dadoDer = "../assets/img/dice" + this.num2 + ".png";
+  }
 }
