23172841a4f8294a3f3887834b3ae67a8e91c3db
Juan Pablo Cesarini <jcesarini@matelab.com.ar>
2022-01-07 14:55:37 -0300

    app terminada

--- app-convertidorMonedas/src/app/components/convertidor/convertidor.component.ts
+++ app-convertidorMonedas/src/app/components/convertidor/convertidor.component.ts
@@ -11,9 +11,55 @@ export class ConvertidorComponent implements OnInit {
   quiero: string = "EUR";
   resultado: number = 0;
   monedas: string[] = ['u$s', 'EUR', '£'];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  convertir(): void {
+    switch (this.tengo) {
+      case 'u$s':
+        switch (this.quiero) {
+          case 'u$s':
+            this.resultado = this.cantidad;
+            break;
+          case 'EUR':
+            this.resultado = this.cantidad * 0.88;
+            break;
+          case '£':
+            this.resultado = this.cantidad * 0.74;
+            break;
+        }
+        break;
+
+      case 'EUR':
+        switch (this.quiero) {
+          case 'u$s':
+            this.resultado = this.cantidad*1.14;
+            break;
+          case 'EUR':
+            this.resultado = this.cantidad
+            break;
+          case '£':
+            this.resultado = this.cantidad * 0.84
+            break;
+        }
+        break;
+
+      case '£':
+         switch (this.quiero) {
+           case 'u$s':
+             this.resultado = this.cantidad * 1.36;
+             break;
+          case 'EUR':
+             this.resultado = this.cantidad * 1.20;
+            break;
+           case '£':
+             this.resultado = this.cantidad;
+             break;
+        }
+        break;
+    }
+  }
 }
