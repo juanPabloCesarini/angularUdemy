5aa9d90c19c66ba186e7534d4b0676081b14ad7d
juanPabloCesarini <juanpcesarini@hotmail.com>
2022-10-30 22:07:49 -0300

    app terminada

--- app-presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ app-presupuesto/src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
@@ -7,14 +8,35 @@ import { PresupuestoService } from 'src/app/services/presupuesto.service';
   styleUrls: ['./listar-gasto.component.css']
 })
 export class ListarGastoComponent implements OnInit {
+  subscription: Subscription;
+  presupuesto: number;
+  saldo: number;
+  listGastos: any[] = [];
 
   constructor(private _presupuestoService: PresupuestoService) { 
-    this._presupuestoService.getGastos().subscribe(data => {
-      console.log(data);
+    this.presupuesto = 0;
+    this.saldo = 0;
+
+    this.subscription = this._presupuestoService.getGastos().subscribe(data => {
+      this.saldo = this.saldo - data.cantidad;
+      this.listGastos.push(data);
     })
   }
 
   ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.saldo = this._presupuestoService.saldo;
   }
 
+    aplicarColorSaldo(){
+      if (this.presupuesto / 4 > this.saldo){
+        return 'alert alert-danger';
+      }else{
+        if (this.presupuesto / 2 > this.saldo){
+          return 'alert alert-warning';
+        }else{
+          return 'alert alert-secondary';
+        }
+      }
+    }
 }
